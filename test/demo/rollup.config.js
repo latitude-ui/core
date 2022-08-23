a8f67073a419bc58f5df4776044510a34f56444f
Ibrahim855 <ibrahimqronfli@outlook.it>
2022-08-23 19:24:39 +0200

    rollup dev server configuration

--- test/demo/rollup.config.js
+++ test/demo/rollup.config.js
@@ -2,7 +2,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-
+import serve from "rollup-plugin-serve";
 
 
 
@@ -19,5 +19,6 @@ export default {
         resolve(),
         commonjs(),
         typescript({tsconfig: 'tsconfig.json'}),
+        serve('./test/demo'),
       ],
 };
