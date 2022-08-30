955f425c0a1dd7dd350ced2c06c4ad8fc96264c0
ibrahim855 <ibrahimqronfli@outlook.it>
2022-08-30 14:10:39 +0200

    disabling linter in demo files

--- test/demo/index.js
+++ test/demo/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { render } from "react-dom";
-import { Button } from "../../src";
+/* eslint-disable  */
+import React from 'react'
+import { render } from 'react-dom';
+import { Button } from '../../src';
 
 const App = () => {
 
-    return <Button onClick={() => alert("ueee")} label="hello world"/>;
+    return <Button onClick={() => alert('ueee')} label='hello world'/>;
 };
 
-
-render(<App />, document.querySelector("#root"));
+render(<App />, document.querySelector('#root'));
